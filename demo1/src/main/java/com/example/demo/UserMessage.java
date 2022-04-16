1b314fb2968e850058330d2de6e5cc1fb059225a
jinshuang <niu.jins@northeastern.edu>
2022-04-15 17:04:53 -0700

    a09

---
+++ demo1/src/main/java/com/example/demo/UserMessage.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class UserMessage {
+    private String username;
+    private String one_time_token;
+    private String link;
+    public String getUsername() {
+        return username;
+    }
+
+    public String getOne_time_token() {
+        return one_time_token;
+    }
+
+    public String getLink() {
+        return link;
+    }
+}
